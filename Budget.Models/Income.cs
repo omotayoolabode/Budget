f31bff0ca17898f1992b9942955b98a4e4efb113
Tayo Olabode <tayo.Olabode@senior.co.uk>
2024-03-16 19:26:41 +0000

    Changed project structure

---
+++ Budget.Models/Income.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Budget.Models
+{
+    public class Income
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Display(Name = "Income Name")]
+        public string IncomeName { get; set; }
+        [Display(Name = "Income Description")]
+        public string IncomeDescription { get; set; }
+    }
+}
---
+++ Budget.Services/DbContext/BudgetDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Budget.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Budget.Services.DbContext;
+
+public class BudgetDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public BudgetDbContext(DbContextOptions<BudgetDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Income> Incomes { get; set; }
+}
